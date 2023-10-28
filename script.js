f10cf9511a594d750480f9c3d80a66b4ec2832be
Viraj <virajshirodkar45@gmail.com>
2023-10-29 02:16:24 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", function () {
+  var row1 = document.querySelector(".row1");
+  var row2 = document.querySelector(".row2");
+
+  if (window.scrollY > 0) {
+    row1.classList.add("sticky");
+    row2.classList.add("sticky");
+  } else {
+    row1.classList.remove("sticky");
+    row2.classList.remove("sticky");
+  }
+});
